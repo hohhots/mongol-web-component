6ea6488c20e9500d4866dbb773ab39d623758075
brgd <hohhots@qq.com>
2019-03-24 16:13:18 +0800

    ad support to mobile

--- packages/mon-body/mon-body.js
+++ packages/mon-body/mon-body.js
@@ -39,7 +39,7 @@ export class MvBody extends LitElement {
     this.mongolWidth = 0;
 
     // browser's horizontal scroll bar height.
-    this.scrollBarHeight;
+    this.scrollBarHeight = 0;
     this.previousScrollBar = false;
 
     this.resizeDelay = 20;
@@ -73,56 +73,36 @@ export class MvBody extends LitElement {
   }
 
   async _firstRendered() {
-    console.log('mvbody first rendered.');
+    console.log('_firstRendered');
+
+    this.parent = this.parentElement;
+    this.mongol = this._root.querySelector(this.mongolId);
 
     if (!this.validMvbody()) {
       return;
     }
 
-    await this.setScrollBarHeight();
+    window.onresize = async() => {
+      console.log('resize window height - ', window.innerHeight);
+      // When window zoom, scroll bar height will change
+      await this.setScrollBarHeight();
 
-    window.onresize = (event) => {
-      if (
-        this.previousScrollBar != this.wHasXScrollBar() ||
-        this.widowHeightChanged()
-      ) {
-        // Temporary disable resize observer for fix bug on firefox
-        this.disableMongolResizeOberver();
-
-        this.triggerResize(event);
-      }
-
-      this.setPreviousDimensions();
+      await this.resizeMongol();
     };
 
-    this.resizeObserver = new ResizeObserver((entries) => {
-      console.log('observe mongol');
-      for (const entry of entries) {
-        this.triggerResize(entry);
-      }
+    this.resizeObserver = new ResizeObserver(async() => {
+      console.log('resize mongol');
+
+      await this.resizeMongol();
     });
-    this.enableMongolResizeOberver();
 
+    this.init();
+  }
+
+  async init() {
+    await this.setScrollBarHeight();
     this.resizeMongol();
-  }
-
-  _didRender() {
-    console.log('_didRender');
-
-    this.parent = this.parentElement;
-    this.mongol = this._root.querySelector(this.mongolId);
-
-    this.resizeThis();
-  }
-
-  triggerResize(event) {
-    clearTimeout(this.resizeTimer);
-
-    this.resizeTimer = setTimeout(() => {
-      this.resizeMongol(event);
-
-      this.enableMongolResizeOberver();
-    }, this.resizeDelay);
+    this.resizeObserver.observe(this.mongol);
   }
 
   resizeBody() {
@@ -133,7 +113,7 @@ export class MvBody extends LitElement {
     // emulate horizontal text html behavior.
     this.parent.parentElement.style.width = `${this.getComputedStyle(
       this.parent,
-      'width'
+      'width',
     ) +
       this.getComputedStyle(this.parent, 'margin-left') +
       this.getComputedStyle(this.parent, 'margin-right')}px`;
@@ -148,36 +128,24 @@ export class MvBody extends LitElement {
     this.resizeBody();
   }
 
-  async resizeMongol(event) {
-    console.log('resize mongol', this.scrollBarHeight, event);
-
-    if (this.mongolSameSize(event)) {
-      return;
-    }
+  async resizeMongol() {
+    console.log('resize mongol');
 
     const th = this.bodyHeight();
+
     await this.setMongolWidth(th);
 
     if (this.wHasXScrollBar()) {
-      this.previousScrollBar = true;
       // mobile browser has no scroll bar height.
-      if (this.scrollBarHeight) {
-        await this.setMongolWidth(th - this.scrollBarHeight);
-      }
-    } else {
-      this.previousScrollBar = false;
+      await this.setMongolWidth(th - this.scrollBarHeight);
     }
 
-    this.setPreviousDimensions();
+    this.resizeThis();
   }
 
   setMongolWidth(width) {
-    console.log('set mongol width - ', width);
-    if (this.mongolWidth != width) {
-      this.mongolWidth = width;
-    } else {
-      this._didRender();
-    }
+    console.log('set mongol width - ', typeof width);
+    this.mongolWidth = width;
   }
 
   parentIsBody() {
@@ -217,18 +185,123 @@ export class MvBody extends LitElement {
   }
 
   bodyHeight() {
+    console.log(`
+      Inner Width: ${window.innerWidth}
+      Inner Height: ${window.innerHeight}
+      Outer Width: ${window.outerWidth}
+      Outer Height: ${window.outerHeight},
+      scrollBarheight: ${this.scrollBarHeight},
+      windowScreenAvailHeight: ${window.screen.availHeight},
+      windowScreenAvailWidth: ${window.screen.availWidth},
+      windowScreenHeight: ${window.screen.height},
+      windowScreenWidth: ${window.screen.width},
+      windowScreenTop: ${window.screen.top},
+      windowScreenLeft: ${window.screen.left},
+    `);
+
     // for browser on pc
-    let height = window.innerHeight;
+    const height = window.innerHeight;
     // for chrome in android and with big content
-    if (window.innerHeight > window.outerHeight) {
-      height = window.outerHeight;
-    }
+    // if (indow.innerHeight > window.outerHeight) {
+    // if (this.isMobile()) {
+    //   console.log('isMobile');
+    //   height = window.outerHeight;
+    //   goInFullscreen();
+    // if (this.isFirefox()) {
+    // height = window.innerHeight;
+    // }
+    //   if (this.isMobileLandscape) {
+    //      console.log('landscaped');
+    //     height = window.innerWidth;
+    //   }
+    // }
 
     return (
       height -
       this.getComputedStyle(this.parent, 'margin-top') -
       this.getComputedStyle(this.parent, 'margin-bottom')
     );
+  }
+
+  goInFullscreen() {
+    console.log('full screen');
+    const element = this.mongol;
+    if (element.requestFullscreen) element.requestFullscreen();
+    else if (element.mozRequestFullScreen) element.mozRequestFullScreen();
+    else if (element.webkitRequestFullscreen) element.webkitRequestFullscreen();
+    else if (element.msRequestFullscreen) element.msRequestFullscreen();
+  }
+
+  isFirefox() {
+    const ua = navigator.userAgent;
+    return ua.toLowerCase().indexOf('firefox') !== -1;
+  }
+
+  isMobile() {
+    const ua = navigator.userAgent;
+    console.log(ua);
+    // memoized values
+    const isIphone =
+      ua.toLowerCase().indexOf('iphone') !== -1 ||
+      ua.toLowerCase().indexOf('ipod') !== -1;
+    const isIpad = ua.toLowerCase().indexOf('ipad') !== -1;
+    const isAndroid = ua.toLowerCase().indexOf('android') !== -1;
+    return isIphone || isIpad || isAndroid;
+  }
+
+  isMobileLandscape() {
+    if (this.isMobile()) {
+      return indow.innerHeight > window.outerHeight;
+    } else {
+      return false;
+    }
+  }
+
+  getUsableHeight() {
+    'use strict';
+
+    // check if this page is within a app frame
+    const isInAppMode =
+      ('standalone' in navigator && navigator.standalone) ||
+      (window.chrome &&
+        window.top.chrome.app &&
+        window.top.chrome.app.isInstalled);
+
+    const ua = navigator.userAgent;
+    // memoized values
+    const isIphone = ua.indexOf('iPhone') !== -1 || ua.indexOf('iPod') !== -1;
+    const isIpad = ua.indexOf('iPad') !== -1;
+    const isAndroid = ua.indexOf('Android') !== -1;
+    const isMobile = isIphone || isIpad || isAndroid;
+
+    // compute the missing area taken up by the header of the web browser to offset the screen height
+    let usableOffset = 0;
+    if (isIphone) {
+      usableOffset = 20;
+    } else if (isAndroid && ua.indexOf('Chrome') === -1) {
+      usableOffset = 1;
+    }
+
+    return function() {
+      if (!isMobile) {
+        return window.innerHeight;
+      }
+      const isLandscape = window.innerWidth > window.innerHeight;
+      let height;
+      // on ios devices, this must use screen
+      if (isIphone) {
+        height = isLandscape ? screen.width : screen.height;
+        if (!isInAppMode) {
+          height -= isLandscape ? 32 : 44;
+          height += 1;
+        }
+      } else {
+        height =
+          (isLandscape ? window.outerWidth : window.outerHeight) /
+          (window.devicePixelRatio || 1);
+      }
+      return height - usableOffset;
+    };
   }
 
   validMvbody() {
@@ -239,73 +312,26 @@ export class MvBody extends LitElement {
     return true;
   }
 
-  enableMongolResizeOberver() {
-    console.log('e-nabled');
-    this.resizeObserver.observe(this.mongol);
-    this.parent.style.overflowY = '';
-  }
-
-  disableMongolResizeOberver() {
-    console.log('d-isabled');
-    this.resizeObserver.disconnect(this.mongol);
-    this.parent.style.overflowY = 'hidden';
-  }
-
-  widowHeightChanged() {
-    if (window.innerHeight != this.previousWindowDimensions.innerHeight) {
-      return true;
-    }
-    return false;
-  }
-
-  setPreviousDimensions() {
-    this.previousWindowDimensions.innerHeight = window.innerHeight;
-
-    this.previousMongolDimension.height = this.getComputedStyle(
-      this.mongol,
-      'height'
-    );
-    this.previousMongolDimension.width = this.getComputedStyle(
-      this.mongol,
-      'width'
-    );
-  }
-
-  mongolSameSize(event) {
-    // if is init or window resize event.
-    if (!event || event.target == window) {
-      return false;
-    } else {
-      // if is mongol div resize event.
-      if (
-        this.previousMongolDimension.height ==
-          this.getComputedStyle(this.mongol, 'height') &&
-        this.previousMongolDimension.width ==
-          this.getComputedStyle(this.mongol, 'width')
-      ) {
-        return true;
-      }
-    }
-    return false;
-  }
-
   setScrollBarHeight() {
-    if (!this.scrollBarHeight) {
-      this.style.display = 'none';
-
-      const div = document.createElement('div');
-      div.style.height = '100vh';
-      div.style.width = '110vw';
-      document.body.appendChild(div);
-
-      window.scrollTo(0, div.scrollHeight);
-      this.scrollBarHeight =
-        Math.ceil(window.pageYOffset) -
-        this.getComputedStyle(this.parent, 'margin-top') -
-        this.getComputedStyle(this.parent, 'margin-bottom');
-      document.body.removeChild(div);
-      this.style.display = '';
+    if (this.isMobile()) {
+      this.scrollBarHeight = 0;
+      return;
     }
+
+    this.style.display = 'none';
+
+    const div = document.createElement('div');
+    div.style.height = '100vh';
+    div.style.width = '110vw';
+    document.body.appendChild(div);
+
+    window.scrollTo(0, div.scrollHeight);
+    this.scrollBarHeight =
+      Math.ceil(window.pageYOffset) -
+      this.getComputedStyle(this.parent, 'margin-top') -
+      this.getComputedStyle(this.parent, 'margin-bottom');
+    document.body.removeChild(div);
+    this.style.display = '';
   }
 }
 
